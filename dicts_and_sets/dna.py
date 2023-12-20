031830cb0a881deea8dc9c41178d99ed44b6ed57
snegirevdv <snegirevdv@gmail.com>
2023-12-20 20:37:42 +0100

    dna

---
+++ dicts_and_sets/dna.py
@@ -0,0 +1,24 @@
+# https://ru.hexlet.io/challenges/python_dicts_to_rna_exercise
+
+# Напишите функцию to_rna, которая принимает на вход цепь ДНК
+# и возвращает соответствующую цепь РНК (совершает транскрипцию РНК)
+
+def to_rna(dna: str) -> str:
+    DNA_TO_RNA = {
+        "G": "C",
+        "C": "G",
+        "T": "A",
+        "A": "U",
+    }
+    return ''.join(DNA_TO_RNA[nt] for nt in dna)
+
+
+def test_to_rna():
+    assert to_rna("C") == "G"
+    assert to_rna("G") == "C"
+    assert to_rna("T") == "A"
+    assert to_rna("A") == "U"
+    assert to_rna("ACGTGGTCTTAA") == "UGCACCAGAAUU"
+
+
+test_to_rna()
