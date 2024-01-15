5cf60d6fb068efe19ea2e1206dfa7cf0607265a0
“Denis <snegirev.dv@gmail.com>
2024-01-16 01:22:09 +0300

    Histogram

---
+++ functions/histogram.py
@@ -0,0 +1,25 @@
+from collections import Counter
+
+from collections.abc import Callable
+
+
+def histo(rounds_count: int, roll_die: Callable) -> str:
+    rolls = (roll_die() for _ in range(rounds_count))
+    stat = Counter(rolls)
+    lines = map(
+        lambda i: f"{i}|" + f"{'#' * stat[i]} {stat[i]}" * bool(stat[i]),
+        range(1, 7)
+    )
+    return "\n".join(lines)
+
+
+def histo(rounds_count: int, roll_die: Callable) -> str:
+    stat = [0] * 6
+    result = []
+    for _ in range(rounds_count):
+        roll = roll_die()
+        stat[roll - 1] += 1
+    for index, count in enumerate(stat):
+        result.append(
+            f"{index + 1}|{('#' * count + ' ' + str(count)) * bool(count)}")
+    return "\n".join(result)
