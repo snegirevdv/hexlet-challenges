b06a386ed268030abb8e6796c28b016fb96b3f6e
“Denis <snegirev.dv@gmail.com>
2024-02-04 00:32:41 +0300

    Queues

---
+++ declarative/queues.py
@@ -0,0 +1,30 @@
+from collections.abc import Iterator
+
+
+def round_robin(*iterables) -> Iterator:
+    iters = [iter(iterable) for iterable in iterables]
+    needs_break = False
+    while not needs_break:
+        needs_break = True
+        for iterator in iters:
+            try:
+                yield next(iterator)
+                needs_break = False
+            except StopIteration:
+                continue
+
+
+def round_robin2(*iterables) -> Iterator:
+    iters = [iter(iterable) for iterable in iterables]
+    needs_break = False
+    while not needs_break:
+        needs_break = True
+        indexes_to_pop = []
+        for index, iterator in enumerate(iters):
+            try:
+                yield next(iterator)
+                needs_break = False
+            except StopIteration:
+                indexes_to_pop.append(index)
+        for delta, index in enumerate(indexes_to_pop):
+            iters.pop(index - delta)
