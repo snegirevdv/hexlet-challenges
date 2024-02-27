f9a1baca58cedbbd4d47732933b260154812ed8f
“Denis <snegirev.dv@gmail.com>
2024-02-27 16:58:13 +0300

    Calculate Probability

---
+++ declarative/calculate_probabilities.py
@@ -0,0 +1,32 @@
+# https://ru.hexlet.io/challenges/python_declarative_programming_probabilities_exercise
+
+def calculate_probabilities(history: list[int]):
+    return {
+        number: {
+            next_number: calculate_probability(number, next_number, history)
+            for next_number
+            in get_next_numbers_list(number, history)
+        } for number in sorted(set(history))
+    }
+
+
+def calculate_probability(number, next_number, history):
+    return sum(
+        1
+        for first, second in zip(history, history[1:])
+        if first == number and second == next_number
+    ) / history[:-1].count(number)
+
+
+def get_next_numbers_list(number, history):
+    return tuple(
+        history[index + 1]
+        for index, value in enumerate(history[:-1])
+        if value == number
+    )
+
+
+
+print(calculate_probabilities([]))
+print(calculate_probabilities([2, 3, 2, 5, 2, 2, 1, 3, 6]))
+print(get_next_numbers_list(6, [2, 3, 2, 5, 2, 2, 1, 3, 6]))
